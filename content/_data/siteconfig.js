ccd58f4c88eeed745bc4fa1837ee9daf601e21b8
Craig McGregor <craigofnz@users.noreply.github.com>
2024-08-25 14:11:42 +1200

    Update siteconfig.js

--- content/_data/siteconfig.js
+++ content/_data/siteconfig.js
@@ -13,7 +13,7 @@ module.exports = {
     author: "Craig McGregor",
     // Site description, shown below site image (optional)
     description:
-        "Personal blog about designing, implementing and securing software and services to operate in the cloud",
+        "Aspiring to get to the gist of all the things.<br/><br/>A personal blog about designing, implementing and securing software and services to operate in the cloud",
     // OpenGraph default image, in case you don't have an `image`
     // set in your Markdown frontmatter; relevant for social
     // sharing.
@@ -23,7 +23,7 @@ module.exports = {
     // LinkedIn ID  (optional, remove it not needed), used for link in the left sidebar
     socialLinkedIn: "craig-mcgregor-9630b863",
     // Twitter ID  (optional, remove it not needed), used for link in the left sidebar, and for OpenGraph sharing information
-    socialTwitter: "craigofnz",
+    socialTwitter: "craigs_gist",
     // YouTube ID/Channel  (optional, remove it not needed), used for link in the left sidebar
     // socialYouTube: "UCH60RRaY2GI9m62z1loLjcA",
     // Google Analytics ID  (optional, remove it not needed), used for... well, Google Analytics
@@ -32,7 +32,7 @@ module.exports = {
     // See: https://github.com/algolia/algoliasearch-netlify
     algoliaSearch: {
         // When enabled shows the search bar in the UI
-        enabled: true,
+        enabled: false,
         // You'll have to set this manually in your build settings.
         // The value comes from Algolia, and is either visible in the
         // UI for the Crawler Plugin or the Algolia Dashboard.
