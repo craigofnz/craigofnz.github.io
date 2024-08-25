8d46d6fa3ebf59396c1f5f9989038dbfbb71aaf2
Craig McGregor <craigofnz@users.noreply.github.com>
2024-08-25 16:02:25 +1200

    Update siteconfig.js thou shouldn't editor on mobile

--- content/_data/siteconfig.js
+++ content/_data/siteconfig.js
@@ -13,9 +13,7 @@ module.exports = {
     author: "Craig McGregor",
     // Site description, shown below site image (optional)
     description:
-        "Aspiring to get to the gist of all the things.
-
-A personal blog about designing, implementing and securing software and services to operate in the cloud",
+        "Aspiring to get to the gist of all the things.\n\nA personal blog about designing, implementing and securing software and services to operate in the cloud",
     // OpenGraph default image, in case you don't have an `image`
     // set in your Markdown frontmatter; relevant for social
     // sharing.
